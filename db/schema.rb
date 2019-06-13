2bf60c5f79f1455a94719f45eaaaf1cd2d8947f3
Mendel Stillerman <menstiler@gmail.com>
2019-06-13 18:06:21 -0400

    added seed file

---
+++ db/schema.rb
@@ -0,0 +1,63 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_06_13_212952) do
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "location"
+    t.string "time"
+    t.integer "student_id"
+    t.integer "teacher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_lessons_on_student_id"
+    t.index ["teacher_id"], name: "index_lessons_on_teacher_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "teacher_id"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_reviews_on_student_id"
+    t.index ["teacher_id"], name: "index_reviews_on_teacher_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "email"
+    t.string "profile_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "profile_image"
+    t.integer "hobby_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hobby_id"], name: "index_teachers_on_hobby_id"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.destroy_all
+Teacher.destroy_all
+Hobby.destroy_all
+Review.destroy_all
+
+emi = Student.create(name: "Emi", age: 30, email: "emi@gmail.com", profile_image: "asfa")
+mendel = Student.create(name: "Mendel", age: 23, email: "mendel@gmail.com", profile_image: "safsd")
+
+puts "creating teachers"
+henry = Teacher.create(name: "Henry", bio: "great teacher", profile_image: "afd", hobby_id: 1)
+jerry = Teacher.create(name: "Jerry", bio: "so so teacher", profile_image: "sdkjfn", hobby_id: 2)
+puts "finished teachers"
+
+
+Hobby.create(name: "Music", category: "Piano")
+Hobby.create(name: "Art", category: "Painting")
+puts "creating lessons"
+Lesson.create(location: "East Village", time: "Saturdays at 10", student_id: mendel.id, teacher_id: henry.id)
+Lesson.create(location: "Brooklyn", time: "Sundays at 5", student_id: emi.id, teacher_id: jerry.id)
+puts "finished lessons"
+
+Review.create(content: "Great teacher", rating: 3.5,  student_id: emi.id, teacher_id: henry.id)
+Review.create(content: "Awesome teacher", rating: 5,  student_id: mendel.id, teacher_id: jerry.id)
