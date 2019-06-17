0592d3fba8d7ebfb0407dbc4be60366c032605a9
Mendel Stillerman <menstiler@gmail.com>
2019-06-17 12:43:23 -0400

    added add review feature

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,4 +1,5 @@
 class TeachersController < ApplicationController
+  before_action :set_teacher, only: [:show, :new_review]
   before_action :authorized
 
   def index
@@ -6,9 +7,26 @@ class TeachersController < ApplicationController
   end
 
   def show
-    @teacher = Teacher.find(params[:id])
     @reviews = @teacher.reviews
   end
 
+  def new_review
+    @student = Student.find(session[:user_id])
+    @review = Review.new(teacher_id: @teacher.id, student_id: @student.id)
+  end
 
+  def create_review
+    @review = Review.create(review_params)
+    redirect_to teacher_path(@review.teacher.id)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :teacher_id, :student_id)
+  end
+
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get '/welcome' => "welcome#home", as: "welcome"
-  get '/book/:id' => "lessons#book"
+  get '/book/:id' => "lessons#book", as: "book_lesson"
   post '/lessons' => "lessons#create_new_lesson"
   get '/lessons' => "lessons#index"
   get '/thank_you' => "lessons#thank_you", as: "thank_you"
   delete '/lessons/:id' => "lessons#cancel_lesson", as: "cancel_lesson"
+  get '/reviews/:id' => "teachers#new_review", as: "new_review"
+  post '/reviews' => "teachers#create_review"
+  delete '/reviews/:id' => "students#delete_review", as: "delete_review"
+
 
   resources :teachers, only: [:index, :show]
   resources :hobbies, only: [:index, :show]
