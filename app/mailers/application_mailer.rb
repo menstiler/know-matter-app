14c2bf17c479de32a00cbe598a2da8a02722fa4b
Mendel Stillerman <menstiler@gmail.com>
2019-08-28 19:14:56 -0400

    using postgres

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-<<<<<<< HEAD
   default from: 'knowmatterapp@gmail.com'
-=======
-  default from: 'knowmatterapp.com'
->>>>>>> master
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,6 @@
 class UserMailer < ApplicationMailer
 
   def welcome_email(user)
-<<<<<<< HEAD
-    # byebug
-=======
->>>>>>> master
     @user = user
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
